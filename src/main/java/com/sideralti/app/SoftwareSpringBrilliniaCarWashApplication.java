82ed7723b3dd3278012049b4a8b986827e19e259
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:20:20 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringBrilliniaCarWashApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringBrilliniaCarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringBrilliniaCarWashApplication.class, args);
+	}
+
+}
