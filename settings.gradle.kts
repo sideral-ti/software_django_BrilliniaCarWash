0ee8b54afd8c31aa93577a14c893a615d8d3732b
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:09:34 -0300

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "software_django_BrilliniaCarWash"
---
+++ src/main/java/com/sideralti/app/SoftwareDjangoBrilliniaCarWashApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDjangoBrilliniaCarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDjangoBrilliniaCarWashApplication.class, args);
+	}
+
+}
